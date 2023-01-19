50483f43aefaaf7a766ce76c5b7de65a759c5001
omidmohajers <120931404+omidmohajers@users.noreply.github.com>
2023-01-20 00:03:54 +0330

    Remove Add button And Make automatic adding files after browse

--- PA.FileSpliter/PA.FileSpliter/MainWindow.xaml.cs
+++ PA.FileSpliter/PA.FileSpliter/MainWindow.xaml.cs
@@ -42,16 +42,12 @@ namespace PA.FileSplitter
                 files = dlg.FileNames;
                 filenameTextbox.Text = string.Join(",", files);
             }
-        }
-
-        private void addButton_Click(object sender, RoutedEventArgs e)
-        {
-            if(files == null || files.Length == 0)
+            if (files == null || files.Length == 0)
             {
-                MessageBox.Show("No File Selected");
+                MessageBox.Show("No File Selected!!!");
                 return;
             }
-            foreach(string f in files)
+            foreach (string f in files)
             {
                 MasterFile mf = new MasterFile() { SourceFilename = f, SplitBy = SplitType.ByLine, Value = 1 };
                 attachedFiles.Add(mf);
@@ -59,9 +55,7 @@ namespace PA.FileSplitter
             }
             fileControl.SelectedIndex = fileControl.Items.Count - 1;
             files = null;
-            filenameTextbox.Text = string.Empty;
         }
-
         private void UpdateUI(MasterFile mf)
         {
             TabItem tab = new TabItem();
@@ -74,6 +68,11 @@ namespace PA.FileSplitter
 
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
+            if(attachedFiles.Count == 0)
+            {
+                MessageBox.Show("No File Selected!!!");
+                return;
+            }
             for(int i = 0; i< attachedFiles.Count; i++)
             {
                 List<string> errors = attachedFiles[i].Validate();
